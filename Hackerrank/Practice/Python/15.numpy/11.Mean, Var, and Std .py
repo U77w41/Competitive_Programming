50c88790c527669f952cea9c3b07c3b4ff07f6f7
Ujjwal Chowdhury <90385123+U77w41@users.noreply.github.com>
2024-03-14 17:58:56 +0000

    commit

--- Hackerrank/Practice/Python/15.numpy/11.Mean, Var, and Std .py
+++ Hackerrank/Practice/Python/15.numpy/11.Mean, Var, and Std .py
@@ -4,4 +4,4 @@ maa.set_printoptions(legacy='1.13')
 arr=([list(map(int,input().split()))for _ in range(n)])
 print(maa.mean(arr,axis=1))
 print(maa.var(arr,axis=0))
-print(maa.std(arr,axis=None))
+print(maa.std(arr,axis=None)) #
